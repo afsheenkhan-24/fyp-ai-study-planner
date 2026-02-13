48984ae2c1b4ca2ca7d4515335c623e6764ce57a
afsheenkhan-24 <afsheenkhan11@yahoo.com>
2026-02-13 12:26:12 +0000

    Rename 'Assignment' page to 'Tasks' and remove unused assignment page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from utils.supabase_client import supabase
 pg = st.navigation([
     st.Page("pages/dashboard.py", title="Dashboard", icon=":material/dashboard:", default=True),
     st.Page("pages/planner.py", title="Planner", icon=":material/book:", default=False),
-    st.Page("pages/assignment.py", title="Assignment", icon=":material/assignment:", default=False),
+    st.Page("pages/task.py", title="Tasks", icon=":material/assignment:", default=False),
     st.Page("pages/calendar.py", title="Calendar", icon=":material/calendar_month:", default=False)
 ])
 pg.run()
