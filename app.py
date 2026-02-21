13290f99f9a526dee17892e7f5c4131b22b580c6
afsheenkhan-24 <afsheenkhan11@yahoo.com>
2026-02-21 09:41:54 +0000

    Add session state (no auth yet)

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from utils.supabase_client import supabase
 
+if "student_id" not in st.session_state:
+    st.session_state.student_id = 1  # Default student ID for testing
+
 pg = st.navigation([
     st.Page("pages/dashboard.py", title="Dashboard", icon=":material/dashboard:", default=True),
     st.Page("pages/planner.py", title="Planner", icon=":material/book:", default=False),
