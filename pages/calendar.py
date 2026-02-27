37df15db32c6b46f0e11dc1d840b11c19e445bf3
afsheenkhan-24 <afsheenkhan11@yahoo.com>
2026-02-27 15:21:53 +0000

    Remove the customised calendar

--- pages/calendar.py
+++ pages/calendar.py
@@ -7,15 +7,3 @@ st.title("Calendar")
 st.date_input("Select a date")
 
 
-available_dates = [
-    datetime.date(2025, 6, 20),
-    datetime.date(2025, 6, 25),
-    datetime.date(2025, 7, 2),
-]
-
-# Call the calendar input
-selected_date = calendar_input(available_dates)
-
-# Display the selected date
-if selected_date:
-    st.success(f"You selected: {selected_date}")
